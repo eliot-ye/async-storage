7e9af31f86453ea44753f1177a6c079071787e6a
Eliot Ye <eliot_ye@163.com>
2024-01-09 12:12:05 +0800

    fix: v1.2.2 - 更改发布库的文件结构

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
 import { createAsyncStorage, EIndexedDB } from "../libs";
 
 const LS = createAsyncStorage(
@@ -15,12 +13,6 @@ const LS = createAsyncStorage(
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
       <button id="counter" type="button"></button>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
+    outDir: "",
     lib: {
       entry: {
         index: resolve(__dirname, "libs/index.ts"),
